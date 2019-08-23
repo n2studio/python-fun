a6bbafee0837198ee5fdbb586429d18007d89a6c
nnguyen <neil.nguyen@navy.mil>
2019-08-22 22:11:46 -0700

    Update print syntax to comply with python 3

--- fib.py
+++ fib.py
@@ -7,7 +7,7 @@ def fib(iterations):
     a,b = 1,1
 
     for i in range(1,iterations+1):
-        print "%d : %d + %d = %d" % (i,a,b,a+b)
+        print ("%d : %d + %d = %d" % (i,a,b,a+b))
         a,b = b,a+b
 
 fib(25)
