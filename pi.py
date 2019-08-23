0762697e84661e958e468b4d39a4fa207eb0db5f
nnguyen <neil.nguyen@navy.mil>
2019-08-22 22:12:15 -0700

    Update print syntax to comply with python 3

--- pi.py
+++ pi.py
@@ -12,7 +12,7 @@ def piNila(iterations):
     pi = Decimal(3)
     term = Decimal(0)
     # account for first iteration, which is only pi = 3
-    print "1 : %.10f" % (pi)
+    print ("1 : %.10f" % (pi))
     for i in range(2,iterations+1):
         term = Decimal(4)/(a*b*c)
         if (i%2 == 0):
@@ -21,12 +21,12 @@ def piNila(iterations):
         else:
             # subtract 
             pi = pi - term
-        print "%d : %.10f" % (i,pi)
+        print ("%d : %.10f" % (i,pi))
         a,b,c = a+2,b+2,c+2
     return pi
 
 piNila(50)
 # Now print the real value of pi for comparison ... 
-print "pi : %.10f" % (math.pi)
-print "Note that even after 50 iterations, the Nilakantha series is only accurate to 5 decimal places. "
-print "100 iterations will yield accuracy to 6 decimal places."
+print ("pi : %.10f" % (math.pi))
+print ("Note that even after 50 iterations, the Nilakantha series is only accurate to 5 decimal places. ")
+print ("100 iterations will yield accuracy to 6 decimal places.")
