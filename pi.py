704f023cf52fc84abfdab9bc4c8c09bc708ae91b
nnguyen <neil.nguyen@navy.mil>
2019-08-28 21:50:35 -0700

    Some code cleanup based on Intellij recommendation.

--- pi.py
+++ pi.py
@@ -7,26 +7,28 @@
 from decimal import *
 import math
 
+
 def piNila(iterations):
-    a,b,c = 2,3,4
+    a, b, c = 2, 3, 4
     pi = Decimal(3)
     term = Decimal(0)
     # account for first iteration, which is only pi = 3
-    print ("1 : %.10f" % (pi))
-    for i in range(2,iterations+1):
-        term = Decimal(4)/(a*b*c)
-        if (i%2 == 0):
+    print("1 : %.10f" % pi)
+    for i in range(2, iterations + 1):
+        term = Decimal(4) / (a * b * c)
+        if i % 2 == 0:
             # add 
             pi = pi + term
         else:
             # subtract 
             pi = pi - term
-        print ("%d : %.10f" % (i,pi))
-        a,b,c = a+2,b+2,c+2
+        print("%d : %.10f" % (i, pi))
+        a, b, c = a + 2, b + 2, c + 2
     return pi
+
 
 piNila(50)
 # Now print the real value of pi for comparison ... 
-print ("pi : %.10f" % (math.pi))
-print ("Note that even after 50 iterations, the Nilakantha series is only accurate to 5 decimal places. ")
-print ("100 iterations will yield accuracy to 6 decimal places.")
+print("pi : %.10f" % (math.pi))
+print("Note that even after 50 iterations, the Nilakantha series is only accurate to 5 decimal places. ")
+print("100 iterations will yield accuracy to 6 decimal places.")
